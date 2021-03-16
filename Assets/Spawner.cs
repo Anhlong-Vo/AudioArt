8f9748a0f109bea8c452000724d9aceef1d6dd08
Anhlong-Vo <anhlong.vo.18@cnu/edu>
2021-03-15 23:11:36 -0400

    Readded the notes, timing circle, and spawner

---
+++ Assets/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject notes;
+    public GameObject timingCircle;
+    public float beat;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > beat)
+        {
+            Vector3 spawn = new Vector3(Random.Range(-1f, 2f), Random.Range(1f, 2f), 15f);
+            Vector3 target = new Vector3(spawn.x, spawn.y, 2f);
+
+            GameObject note = Instantiate(notes, spawn, Quaternion.identity);
+            GameObject circle = Instantiate(timingCircle, target, Quaternion.identity);
+
+            timer -= beat;
+
+            Destroy(note, 3f);
+        }
+        timer += Time.deltaTime;
+        
+    }
+
+}
---
+++ Assets/TimingCircle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimingCircle : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Timing circle slowly shrinks in size
+        transform.localScale -= new Vector3(Time.deltaTime/4, Time.deltaTime/4,  Time.deltaTime/4);
+
+        if (transform.localScale.x < 0.3f)
+        {
+            Destroy(transform.gameObject);
+        }
+
+        if (transform == null)
+        {
+            Debug.Log("Circle is gone");
+        }
+    }
+}
